b761d375d2d2e8f97f904c8d8e24c65d31ceb6d7
Thomas England <tengland808@gmail.com>
2024-08-03 16:05:50 -1000

    added assets
    assets were added

---
+++ Source/Aura/Private/Characters/AuraCharacterBase.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/AuraCharacterBase.h"
+
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+
+	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
