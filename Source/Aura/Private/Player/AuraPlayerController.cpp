561badb257d0cfad51855e5fd58e8108676f219b
Thomas England <tengland808@gmail.com>
2024-08-04 16:42:34 -1000

    update
    update

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,11 +3,23 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+	PrimaryActorTick.bCanEverTick = true;
+
 }
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+	CursorTrace();
+
+
+}
+
 
 void AAuraPlayerController::BeginPlay()
 {
@@ -54,4 +66,61 @@ void AAuraPlayerController::Move(const FInputActionValue &InputActionValue)
 		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
 	}
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+
+	if (!CursorHit.bBlockingHit) return;
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+
+
+	/*
+	* Line trace from cusror. There are several scenarios:
+	* a. LastActor is null && This actor is null
+	*	- do nothing or return -- DONE [else]
+	* b. LastActor is null && ThisActor is valid
+	*	- call HighlightActor on ThisActor -- DONE
+	* c. LastActor is valid && ThisActor is null
+	*	- Call UnHighlightActor on LastActor -- DONE
+	* d. LastActor is Valid && ThisActor is valid and lastactor != thisactor
+	*	- call UnhighlightActor on LastActor and HighlightActor on ThisActor -- DONE
+	* e. LastActor is Valid && ThisActor is valid and lastactor == thisactor
+	*	do nothing -- DONE [else]
+	*/
+
+	if (LastActor.GetInterface() == nullptr)
+	{
+		if (ThisActor.GetInterface())
+		{
+			// case b.
+			ThisActor->HighlightActor();
+		}
+		else
+		{
+			// ThisActor is null
+		}
+	}
+	else //LastActor is valid
+	{
+		if (ThisActor.GetInterface() == nullptr)
+		{
+			// case c
+			LastActor->UnHighlightActor();
+		}
+		else
+		{
+			// both actors are valid
+			if (LastActor.GetInterface() != ThisActor.GetInterface())
+			{
+				// case d
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+		}
+	}
+
 }
--- Source/Aura/Public/Characters/AuraEnemy.h
+++ Source/Aura/Public/Characters/AuraEnemy.h
@@ -4,16 +4,22 @@
 
 #include "CoreMinimal.h"
 #include "Characters/AuraCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacterBase
+class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 	
+
+public:
+	AAuraEnemy();
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
 	
 	
 	
